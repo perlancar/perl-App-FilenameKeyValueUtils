5430dadffbe1463ec4da7f9682db9f498a3302f3
perlancar <perlancar@gmail.com>
2026-05-28 17:40:13 +0700

    Add utilities from Filename::KeyValue 0.003+: modify-keyvalue-filename, normalize-keyvalue-filename, rename-add-keyvalue-filenames, rename-remove-keyvalue-filenames

---
+++ lib/App/FilenameKeyValueUtils.pm
@@ -0,0 +1,28 @@
+package App::FilenameKeyValueUtils;
+
+use strict;
+use warnings;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+our %SPEC;
+
+1;
+# ABSTRACT: CLIs for Filename::KeyValue
+
+=head1 SYNOPSIS
+
+
+=head1 DESCRIPTION
+
+This distribution includes several utilities related to L<Filename::KeyValue>:
+
+#INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
+
+L<Filename::KeyValue>
